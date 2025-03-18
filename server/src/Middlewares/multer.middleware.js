9e1000a2eb767a2b85b903980951536e5d2ee664
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-03-18 17:03:50 +0530

    fix: update multer middleware to use path.join for dynamic upload path resolution

--- server/src/Middlewares/multer.middleware.js
+++ server/src/Middlewares/multer.middleware.js
@@ -1,10 +1,11 @@
 import multer from 'multer'
+import path from 'path'
 
 const storage = multer.diskStorage({
     destination: function (req, res, cb) {
+      // cb(null, './public/temp')
       const uploadPath = path.join(process.cwd(), 'public', 'temp');
       cb(null, uploadPath);
-      // cb(null, '../../public/temp')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
