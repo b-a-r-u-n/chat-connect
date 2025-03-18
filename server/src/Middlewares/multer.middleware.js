9e43f848fad7c283d8a91989159b725f78720b04
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2025-03-18 16:46:31 +0530

    fix: restore multer middleware storage destination path to './public/temp'

--- server/src/Middlewares/multer.middleware.js
+++ server/src/Middlewares/multer.middleware.js
@@ -2,8 +2,8 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
     destination: function (req, res, cb) {
-      // cb(null, './public/temp')
-      cb(null, '../../public/temp')
+      cb(null, './public/temp')
+      // cb(null, '../../public/temp')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
